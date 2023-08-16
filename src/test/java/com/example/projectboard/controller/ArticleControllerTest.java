6c39251393e0ffe66a9745be72da2e8429fdad1c
Kwon yongho <b2b2004@nate.com>
2023-08-16 21:01:25 +0900

    #17 - 게시글, 게시판 뷰 페이지 만들기
    게시글, 게시판 뷰 페이지 만들기

--- src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -35,7 +35,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("view [GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void article() throws Exception {
@@ -50,7 +49,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articleComments"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("view [GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     public void articleSearch() throws Exception {
@@ -63,7 +61,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles/search"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("view [GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     public void articleSearchHashtag() throws Exception {
